8979afa964c660969712069322dc9a7a4c03093a
crohnaro <crohnarodev@gmail.com>
2023-05-04 00:44:02 -0300

    feat: :lipstick: creating the initial UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Button from "@/components/ui/Button"
+import { db } from "@/lib/db"
 
 
-export default function Home() {
+export default async function Home() {
+
+  await db.set('hello', 'hello')
   return (
-    <div className='text-red-500'>hello world</div>
+    <Button variant='ghost'>Hello</Button>
   )
 }
