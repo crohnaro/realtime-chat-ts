faf12d7cf20fdb9c7e4d1fdcec8dd3539dd59eaa
crohnaro <crohnarodev@gmail.com>
2023-05-04 00:43:35 -0300

    docs: :label: adding env variables to types to be recognised and used

---
+++ types-env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        UPSTASH_REDIS_REST_URL: string,
+        UPSTASH_REDIS_REST_TOKEN: string,
+        GOOGLE_CLIENT_ID: string,
+        GOOGLE_CLIENT_SECRET: string
+    }
+  }
